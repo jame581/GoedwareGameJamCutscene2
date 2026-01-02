568fbd5615ce7d860acbf3d4c8439fc958054f4c
Jan Mesarč <jame_581@windowslive.com>
2026-01-02 22:26:27 +0100

    Enhance camera movement script to reset rotation on additional mouse button inputs

--- project/scripts/camera_movement.gd
+++ project/scripts/camera_movement.gd
@@ -7,5 +7,5 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		rotation += deg_to_rad(event.relative.x * rotation_speed)
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT or event.double_click:
+		if event.button_index == MOUSE_BUTTON_LEFT or event.double_click or event.button_index == MOUSE_BUTTON_RIGHT or event.button_index == MOUSE_BUTTON_MIDDLE:
 			rotation = 0
