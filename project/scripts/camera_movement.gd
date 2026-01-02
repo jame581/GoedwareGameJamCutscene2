d3593f1a123b89e9054c712cc6b32e04b55b4bf4
Jan Mesarč <jame_581@windowslive.com>
2026-01-02 22:19:56 +0100

    Add camera movement script to handle rotation and reset on input events

---
+++ project/scripts/camera_movement.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+
+@export var rotation_speed: float = 0.1
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation += deg_to_rad(event.relative.x * rotation_speed)
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT or event.double_click:
+			rotation = 0
