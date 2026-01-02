7955cec7a7501882fde983c13dcd326f45d6a604
Jan Mesarč <jame_581@windowslive.com>
2026-01-02 22:51:52 +0100

    Base main menu scene layout and version display

--- project/scripts/main_menu.gd
+++ project/scripts/main_menu.gd
@@ -1,11 +1,20 @@
-extends Node
+extends Control
+
+@onready var version_label: Label = %VersionLabel
+@onready var start_button: Button = %StartButton
+@onready var audio_stream_player: AudioStreamPlayer = get_node("AudioStreamPlayer")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	version_label.text = Global.get_version()
+
+	start_button.pressed.connect(_on_start_button_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_start_button_pressed() -> void:
+	Global.goto_scene("res://maps/storyboard.tscn")
